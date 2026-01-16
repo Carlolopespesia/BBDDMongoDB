f78cf251e1c44bb7226c7be7cb073c437cd11884
Carlolopespesia <125183410+Carlolopespesia@users.noreply.github.com>
2026-01-16 14:13:20 +0100

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Main {
+    static void main() {
+        try {
+            org.example.ConnectionBBDD.getConnectionMongo();
+            System.out.println("Conexión correcta a PostgreSQL.");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        Alumno alumno = new Alumno();
+
+        // Llama al metodo para ver los alumnos
+        alumno.listarAlumnos();
+        // Insertar org.example.Alumno
+        alumno.insertAlumno("ana", "Vázquez", "Guerrero", 25);
+        // Actualizar org.example.Alumno
+        alumno.actualizarAlumno("Mónica", "Mónica", "Blásquez", "Güera", 20);
+        // Borrar org.example.Alumno
+        //alumno.deleteByName("ana");
+    }
+}
